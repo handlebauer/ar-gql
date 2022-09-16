ac04ba6c4ab949dd536ef1d995771a291565b7f4
Donald Geddes <hi@donaldgeddes.ca>
2022-09-16 02:22:02 -0700

    Force dark mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,6 +51,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+        colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: true,
+        respectPrefersColorScheme: false,
+      },
       navbar: {
         title: 'ArGQL Guide',
         logo: {
