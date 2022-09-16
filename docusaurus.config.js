d076d595ba3526ebe1c22d4d362d189a443a24a1
Donald Geddes <hi@donaldgeddes.ca>
2022-09-16 13:02:51 -0700

    Fix spelling

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'ArGQL Guide',
   tagline: 'Arweave GraphQL Guide',
-  url: 'ar-gql.vercell.app',
+  url: 'ar-gql.vercel.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
