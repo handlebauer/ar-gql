f2c382db4f2b4b9712fb38c4537a0ebd8d5aec5c
Donald Geddes <hi@donaldgeddes.ca>
2022-09-19 22:21:19 -0700

    Correct URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'ArGQL Guide',
   tagline: 'Arweave GraphQL Guide',
-  url: 'https://argql.vercel.app',
+  url: 'https://arweave-gql.vercel.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
